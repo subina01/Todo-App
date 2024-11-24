076f57234fbd97f602302ab48df0a9e790e0bc6f
Subina Dhakal <subinadhakal01@gmail.com>
2024-11-24 14:34:54 +0545

    feat(core): complete core layer implementation
    Implemented the core layer with all required business logic
    and entities. This layer acts as the foundation for the
    application and ensures separation of concerns.

--- TodoApp.Core/Domain/Interface/ITodoServices.cs
+++ TodoApp.Core/Domain/Interface/ITodoServices.cs
@@ -10,57 +10,89 @@ namespace TodoApp.Core.Domain.Interface
 
 {
 
-
-        public interface ITodoServices
-        {
-                public Task AddTask(ToDo ToDoData);
-                public Task<UpdateTaskResponseDTO> UpdateTask(int id, UpdateTaskRequestDTO updateTodoTask);
-                public Task<GetAllTaskResponseDTO> GetAllTask();
-                public Task<GetTaskByIdResponseDTO> GetTaskById(int id);
-
-
-                public Task<DeleteTaskResponseDTO> DeleteTask(int id);
-
-        }
-
-
-
         public interface ITodoServices
         {
 
-
-                public Task<IEnumerable<GetAllTasksResponseDTO>> GetAllTasks();
 
                 public Task<IEnumerable<GetAllTasksResponseDTO>> GetAllTasks();
 
                 public Task<GetAllTaskByIdResponseDTO> GetTaskById(int id);
+                public interface ITodoServices
+                {
+                        public Task AddTask(ToDo ToDoData);
+                        public Task<UpdateTaskResponseDTO> UpdateTask(int id, UpdateTaskRequestDTO updateTodoTask);
+                        public Task<GetAllTaskResponseDTO> GetAllTask();
+                        public Task<GetTaskByIdResponseDTO> GetTaskById(int id);
 
+
+                        public Task<DeleteTaskResponseDTO> DeleteTask(int id);
+
+                }
+
+
+
+                public interface ITodoServices
+                {
+
+
+                        public Task<IEnumerable<GetAllTasksResponseDTO>> GetAllTasks();
+
+                        public Task<IEnumerable<GetAllTasksResponseDTO>> GetAllTasks();
+
+                        public Task<GetAllTaskByIdResponseDTO> GetTaskById(int id);
+
+
+                        public Task AddTask(ToDo tododata);
+                        public Task<UpdateTaskResponseDTO> UpdateTask(int id, UpdateTaskRequestDTO updateTodoTask);
+                        public Task<UpdateStatusResponseDTO> UpdateTaskStatus(int id, UpdateStatusRequestDTO newStatus);
+                        public Task<DeleteTaskResponseDTO> DeleteTask(int id);
+                }
+
+
+                public interface ITodoServices
+                {
+
+                        public Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
+
+                        public Task<GetAllTaskByIdResponseDTO> GetTaskById(int id);
+
+
+
+
+
+
+                        public Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
+                        public Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
+                        public Task<IEnumerable<GetAllTasksResponseDTO>> GetAllTasks();
+
+
+                        public Task<GetAllTaskByIdResponseDTO> GetTaskById(int id);
+
+                        Task AddTask(ToDo tododata);
+                        Task<string> UpdateTask(int id, TodoResponseDTO updateTodoTask);
+                        Task<string> UpdateTaskStatus(int id, TodoResponseDTO newStatus);
+                        Task<string> DeleteTask(int id);
+
+                        public Task AddTask(ToDo tododata);
+                        public Task<UpdateTaskResponseDTO> UpdateTask(int id, UpdateTaskRequestDTO updateTodoTask);
+                        public Task<UpdateStatusResponseDTO> UpdateTaskStatus(int id, UpdateStatusRequestDTO newStatus);
+                        public Task<DeleteTaskResponseDTO> DeleteTask(int id);
+                }
+                Task AddTask(ToDo tododata);
+                Task<string> UpdateTask(int id, TodoResponseDTO updateTodoTask);
+                Task<string> UpdateTaskStatus(int id, TodoResponseDTO newStatus);
+                Task<string> DeleteTask(int id);
 
                 public Task AddTask(ToDo tododata);
                 public Task<UpdateTaskResponseDTO> UpdateTask(int id, UpdateTaskRequestDTO updateTodoTask);
                 public Task<UpdateStatusResponseDTO> UpdateTaskStatus(int id, UpdateStatusRequestDTO newStatus);
                 public Task<DeleteTaskResponseDTO> DeleteTask(int id);
+
         }
+     public Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
 
+                Task<TodoResponseDTO> GetTaskById(int id);
 
-        public interface ITodoServices
-        {
-
-                public Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
-
-                public Task<GetAllTaskByIdResponseDTO> GetTaskById(int id);
-
-
-
-
-
-
-                public Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
-                public Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
-                public Task<IEnumerable<GetAllTasksResponseDTO>> GetAllTasks();
-
-
-                public Task<GetAllTaskByIdResponseDTO> GetTaskById(int id);
 
                 Task AddTask(ToDo tododata);
                 Task<string> UpdateTask(int id, TodoResponseDTO updateTodoTask);
@@ -71,15 +103,3 @@ namespace TodoApp.Core.Domain.Interface
                 public Task<UpdateTaskResponseDTO> UpdateTask(int id, UpdateTaskRequestDTO updateTodoTask);
                 public Task<UpdateStatusResponseDTO> UpdateTaskStatus(int id, UpdateStatusRequestDTO newStatus);
                 public Task<DeleteTaskResponseDTO> DeleteTask(int id);
-        }
-        Task AddTask(ToDo tododata);
-                Task<string> UpdateTask(int id, TodoResponseDTO updateTodoTask);
-                Task<string> UpdateTaskStatus(int id, TodoResponseDTO newStatus);
-                Task<string> DeleteTask(int id);
-
-                public Task AddTask(ToDo tododata);
-                public Task<UpdateTaskResponseDTO> UpdateTask(int id, UpdateTaskRequestDTO updateTodoTask);
-                public Task<UpdateStatusResponseDTO> UpdateTaskStatus(int id, UpdateStatusRequestDTO newStatus);
-                public Task<DeleteTaskResponseDTO> DeleteTask(int id);
-
-        }
