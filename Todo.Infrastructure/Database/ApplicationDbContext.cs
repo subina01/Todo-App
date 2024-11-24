a4127334c1fd754e77ec4bf1278162789900a49b
Subina Dhakal <subinadhakal01@gmail.com>
2024-11-24 14:42:47 +0545

    feat(infrastructure): add application db context and service implementation
    Created the infrastructure layer with the ApplicationDbContext for database interactions
    and implemented necessary services to support the core layer. This establishes the
    connection between the application and the database.

--- Todo.Infrastructure/Database/ApplicationDbContext.cs
+++ Todo.Infrastructure/Database/ApplicationDbContext.cs
@@ -4,7 +4,20 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
->>>>>>> c90400b(feat(Registration): Added Registration API using ASP.NET Identity)
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -48,14 +61,24 @@ namespace TodoApp.Infrastructure.Database
 
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<ToDo> TodoData { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+namespace Todo.Infrastructure.Database
+    {
+        public class ApplicationDbContext : DbContext
+
+using TodoApp.Core.Domain.IdentityEntities;
+
+namespace Todo.Infrastructure.Database
         {
-            modelBuilder.Entity<ToDo>().HasKey(t => t.Id);
-            base.OnModelCreating(modelBuilder);
+            public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+            {
+                public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+                public DbSet<ToDo> TodoData { get; set; }
+
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+                {
+                    modelBuilder.Entity<ToDo>().HasKey(t => t.Id);
+                    base.OnModelCreating(modelBuilder);
+                }
+            }
         }
-    }
-}
--- Todo.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Todo.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -124,40 +124,39 @@ namespace Todo.Infrastructure.Migrations
 
                     b.ToTable("AspNetUserTokens", (string)null);
                 });
-
             modelBuilder.Entity("TodoApp.Core.Domain.Entities.ToDo", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+                  {
+                      b.Property<int>("Id")
+                          .ValueGeneratedOnAdd()
+                          .HasColumnType("int");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                      SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                    b.Property<DateTime>("CompletionDate")
-                        .HasColumnType("datetime2");
+                      b.Property<DateTime>("CompletionDate")
+                          .HasColumnType("datetime2");
 
-                    b.Property<string>("Description")
-                        .HasColumnType("nvarchar(max)");
+                      b.Property<string>("Description")
+                          .HasColumnType("nvarchar(max)");
 
-                    b.Property<DateTime>("DueDate")
-                        .HasColumnType("datetime2");
+                      b.Property<DateTime>("DueDate")
+                          .HasColumnType("datetime2");
 
-                    b.Property<DateTime>("StartDate")
-                        .HasColumnType("datetime2");
+                      b.Property<DateTime>("StartDate")
+                          .HasColumnType("datetime2");
 
-                    b.Property<string>("Status")
-                        .HasColumnType("nvarchar(max)");
+                      b.Property<string>("Status")
+                          .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("TaskType")
-                        .HasColumnType("nvarchar(max)");
+                      b.Property<string>("TaskType")
+                          .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("UserName")
-                        .HasColumnType("nvarchar(max)");
+                      b.Property<string>("UserName")
+                          .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+                      b.HasKey("Id");
 
-                    b.ToTable("TodoData");
-                });
+                      b.ToTable("TodoData");
+                  });
 
 
 
@@ -318,10 +317,6 @@ namespace Todo.Infrastructure.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
-
-
-
-
 
         }
     }
