2e54e6a030108bdbd8bb0889bd64b94fd54baf3b
Subina Dhakal <subinadhakal01@gmail.com>
2024-11-26 10:53:34 +0545

    feat(dto):add request and response DTOs for updating, deleting, and retrieving tasks

--- TodoApp.Core/DTO/UpdateTaskRequestDTO.cs
+++ TodoApp.Core/DTO/UpdateTaskRequestDTO.cs
@@ -6,21 +6,19 @@ using System.Threading.Tasks;
 
 namespace TodoApp.Core.DTO
 {
-    public class TodoResponseDTO
+    public class UpdateTaskRequestDTO
     {
         public int Id { get; set; }
+
         public string? UserName { get; set; }
         public string? TaskType { get; set; }
 
         public string? Description { get; set; }
 
-        public DateTime StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
 
-        public DateTime DueDate { get; set; }
+        public DateTime? DueDate { get; set; }
 
-        public DateTime CompletionDate { get; set; }
-
-        public string? Status { get; set; }
-
+        public DateTime? CompletionDate { get; set; }
     }
 }
