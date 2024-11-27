c90400b2f73bdcee9775f5e4b83a74f3e4c5f647
Subina Dhakal <subinadhakal01@gmail.com>
2024-11-27 14:34:31 +0545

    feat(Registration): Added Registration API using ASP.NET Identity

--- Todo.Infrastructure/Database/ApplicationDbContext.cs
+++ Todo.Infrastructure/Database/ApplicationDbContext.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoApp.Core.Domain.Entities;
+using TodoApp.Core.Domain.IdentityEntities;
 
 namespace Todo.Infrastructure.Database
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<ToDo> TodoData { get; set; }
