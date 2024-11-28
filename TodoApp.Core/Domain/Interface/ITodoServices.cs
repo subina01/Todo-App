71dda87223c0c187695e32cbde8e7c4a9b89c981
Subina Dhakal <subinadhakal01@gmail.com>
2024-11-28 20:55:36 +0545

    refactor: updated sln file with updatedfeatures

---
+++ TodoApp.Core/Domain/Interface/ITodoServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Core.Domain.Entities;
+using TodoApp.Core.DTO;
+
+namespace TodoApp.Core.Domain.Interface
+
+{
+    public interface ITodoServices
+    {
+
+        public  Task<IEnumerable<TodoResponseDTO>> GetAllTasks();
+
+        Task<TodoResponseDTO> GetTaskById(int id);
+
+
+        Task AddTask(ToDo tododata);
+        Task<string> UpdateTask(int id, TodoResponseDTO updateTodoTask);
+        Task<string> UpdateTaskStatus(int id, TodoResponseDTO newStatus);
+        Task<string> DeleteTask(int id);
+    }
+}
