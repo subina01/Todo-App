a978e7ab9c39df5713c0eeec1a418420d3e5d8c8
Subina Dhakal <subinadhakal01@gmail.com>
2024-12-01 15:17:23 +0545

    chore: Remove bin and obj folders from version control

--- TodoApp.Core/Domain/IdentityEntities/ApplicationUser.cs
+++ TodoApp.Core/Domain/IdentityEntities/ApplicationUser.cs
@@ -27,7 +27,14 @@ using TodoApp.Core.Domain.Enum;
 
 using TodoApp.Core.Domain.Enum;
 
+>>>>>>> 6af2c4c(feat(auth): implement authentication and authorization using jwt)
+=======
+<<<<<<< HEAD
 
+>>>>>>> 6af2c4cf890176350c27058ed130352aecbf6b7c
+=======
+>>>>>>> 73d3215 (feat(Registration): Added Registration API using ASP.NET Identity)
+>>>>>>> be0f8e4(chore: Remove bin and obj folders from version control)
 
 namespace TodoApp.Core.Domain.IdentityEntities
 {
@@ -42,6 +49,22 @@ namespace TodoApp.Core.Domain.IdentityEntities
 
 
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+     
+=======
+>>>>>>> c90400b (feat(Registration): Added Registration API using ASP.NET Identity)
+=======
+     
+>>>>>>> 86a57ab (feat(auth): Add jwt based authentication)
+=======
+<<<<<<< HEAD
+     
+>>>>>>> 6af2c4cf890176350c27058ed130352aecbf6b7c
+=======
+>>>>>>> 73d3215 (feat(Registration): Added Registration API using ASP.NET Identity)
+>>>>>>> be0f8e4(chore: Remove bin and obj folders from version control)
     }
 }
 
