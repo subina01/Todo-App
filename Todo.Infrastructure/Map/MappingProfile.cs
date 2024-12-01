959e87c9c0f699608c8cd8e05616af1defa9f77c
Subina Dhakal <subinadhakal01@gmail.com>
2024-12-01 13:19:46 +0545

    refactor(repository):Implement mapping between DTO using AutoMapper

---
+++ Todo.Infrastructure/Map/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TodoApp.Core.Domain.Entities;
+using TodoApp.Core.DTO;
+
+namespace Todo.Infrastructure.Map
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ToDo, TodoResponseDTO>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<UpdateTaskRequestDTO, ToDo>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+        }
+    }
+}
